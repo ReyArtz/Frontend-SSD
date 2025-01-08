5d8b2e873104e57563b882c153bb3894f3309a4c
ReyArtz <andrei.mage22@gmail.com>
2025-01-09 00:07:25 +0200

    start-up

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyDXeLbEHK__q0CLqZhiks5_vkX4RciYDTA",
+      authDomain: "ssd0-68e95.firebaseapp.com",
+      projectId: "ssd0-68e95",
+      storageBucket: "ssd0-68e95.appspot.com",
+      messagingSenderId: "439034400625",
+      appId: "1:439034400625:web:0d71a5ed0d01a3334970d0",
+      measurementId: "G-WSCBMT162F"
+    }
+  };
+  
